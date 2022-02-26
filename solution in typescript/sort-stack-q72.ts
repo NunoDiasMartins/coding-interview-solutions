d079d1db45bb5c19b94e3dfbd74e93aead8e270b
Nuno Dias Martins <NunoDiasMartins@users.noreply.github.com>
2022-02-26 10:58:26 -0500

    Solution for sort stack using stack properties

---
+++ solution in typescript/sort-stack-q72.ts
@@ -0,0 +1,22 @@
+export function sortStack(stack: number[]) {
+    return stack.length === 0 ? [] : this.sortStackAux(stack, stack.pop());
+}
+
+export function sortStackAux(stack: number[], previousElement: number): number[] {
+    console.log(1);
+    if (stack.length === 0) {
+        stack.push(previousElement);
+        return stack;
+    }
+    const currentElement: number = stack.pop()!;
+    const min = Math.min(previousElement, currentElement);
+    const max = Math.max(previousElement, currentElement);
+    this.sortStackAux(stack, min);
+
+    if (stack[stack.length - 1] > max) {
+        this.sortStackAux(stack, max);
+    } else {
+        stack.push(max);
+    }
+    return stack;
+}
